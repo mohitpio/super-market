12c58692935db802b14b3a1edd566a1e825604a7
mohit kachhwaha <mohitpio713@gmail.com>
2022-07-01 08:32:26 +0530

    my commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+
 const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
@@ -19,7 +20,7 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`, mongoString)
+    console.log(`Server Started at ${3000}`)
 })
 
 const routes = require('./routes/routes');
